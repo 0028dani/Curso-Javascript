e44440a2e7c048938038a4c9d7ccfc4b499931e4
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-08-25 16:22:18 -0300

    Variavel do tipo string

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá,mundo!';
+const senha = "senhaSuperSeguro456";
+const StringDeNumero = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string OU template literal
