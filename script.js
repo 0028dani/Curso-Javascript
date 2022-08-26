f47ba7294d10dbb8811d2e6aae83d1b8cac0799e
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-08-26 12:15:29 -0300

    Diferenças de var, let e const

---
+++ script.js
@@ -0,0 +1,35 @@
+// var
+var altura = 8;
+var comprimento = 7;
+
+area = altura * comprimento;
+console.log(area)
+var area;
+
+// let
+let forma = 'retângulo'
+let altura1 = 7;
+let comprimento1 = 7;
+let area1;
+
+if (forma === 'retângulo') {
+    area1 = altura1 * comprimento1;
+} else {
+    area1= (altura1 * comprimento1) / 2;
+}
+
+console.log(area1)
+
+// const
+const forma2 = 'quadrado';
+const altura2 = 5;
+const comprimento2 =7;
+let area2;
+
+if (forma2 === 'quadrado'){
+    area2 = altura2 * comprimento2;
+}else {
+    area2 = (altura2 * comprimento2) / 2;
+}
+
+console.log(area2)
