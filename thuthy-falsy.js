1335eb09fb066e4d9cb0a8689672a37144484918
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-08-26 15:14:19 -0300

    thuthy or falsy do javascript e o tipo typeof

---
+++ thuthy-falsy.js
@@ -0,0 +1,33 @@
+//boolean
+const usuarioLogado = true;
+const contapaga = false;
+
+// thuthy or false
+
+// 0 => false
+// 1 => true
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+// null 
+// undefined
+
+// null ===> vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+// typeoff qual tipo de dado guardado na variavel
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero)
+console.log(typeof texto)
+console.log(typeof minhaVar)
+console.log(typeof varNull)
