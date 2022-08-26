1aa5a668ac9f12981b4bf96891bf4199654e04fd
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-08-26 15:39:53 -0300

    Conversoes implícita e explícita

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// Tipo de dado
+// booleanos
+
+// conversão implícita
+const numero = 456;
+const numeroString1 = "456";
+// const numeroString = Number "456";
+ const numeroString = Number ("456a");
+
+console.log(numero + numeroString1)
+
+// conversão explícita
+console.log(numero + Number(numeroString1))
+console.log(numero + (numeroString))
