ac74a2ddb5c3df2b7b4683b7b81e626a1a881096
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-08-30 15:27:52 -0300

    Comparação dos operadores == e ===

---
+++ comparacoes.js
@@ -0,0 +1,25 @@
+// == (comparação explícita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto)
+
+// ===
+const numero1 = 5;
+const texto1 = "5";
+
+console.log(numero1 === texto1)
+
+//typeof
+console.log(typeof numero1)
+console.log(typeof texto1)
+
+
+// == só comparar o valor
+// === compara o valor e o tipo de dado
+
+// conversão explícita
+
+Number()
+String()
