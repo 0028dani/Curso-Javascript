24f0a238f9424e13ea6e1a232684df2c6fcbbe3c
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-08-30 15:27:03 -0300

    Mais tipos de boolean

--- type-boolean.js
+++ type-boolean.js
@@ -10,3 +10,15 @@ const texto1 = "Alura";
 const texto2 = "alura";
 
 console.log(texto1 === texto2)
+
+// outros tipos de boolean
+const numero2 = 5;
+const segundoNumero2 = 5;
+
+console.log(numero2 == segundoNumero2) // igual
+console.log(numero2 != segundoNumero2); // diferente
+console.log(numero2 !== segundoNumero2); // não indetifico
+console.log(numero2 < segundoNumero2); // menor que
+console.log(numero2 > segundoNumero2); // maior que
+console.log(numero2 <= segundoNumero2); // menor ou igual
+console.log(numero2 >= segundoNumero2); // maior ou igual
