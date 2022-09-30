c1af0dac515a0ccff86dbbcb157a0f0ffd065357
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-09-29 23:03:22 -0300

    array de duas dimensões

---
+++ array-lista-duas-dimensoes.js
@@ -0,0 +1,8 @@
+               //0
+const alunos = ['João', ' Juliana', ' Caio', ' Ana']
+                       //0
+const mediaDosAlunos = [10, 7, 9, 6]
+                          //0     //1
+let listaDeNotasEAlunos = [alunos, mediaDosAlunos]
+
+console.log(`${listaDeNotasEAlunos[0][0]}, sua media é ${listaDeNotasEAlunos[1][0]}`)
