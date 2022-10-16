c49cc5e9554afbfad92509cf98cd860740170e87
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-10-16 12:47:59 -0300

    Objeto cliente

---
+++ Objetos/objetos.js
@@ -0,0 +1,10 @@
+const listaCPFs = ["1234566544", "234456767655", "122233445666"]
+
+//const cliente = ["nome", "André", "idade" ,36]
+
+const cliente = {
+    nome: "André",
+    idade: 36,
+    cpf: "123456789",
+    email: "andre@email.com"
+}
