9c619988804dca85c68a1f87e7f9310c5648f59c
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-10-16 12:47:14 -0300

    expressoes funcao

---
+++ expressoes-funcao.js
@@ -0,0 +1,25 @@
+// declaração de função
+
+//function minhaFuncao(param) {
+//    // bloco de codigos
+//} 
+//
+//minhaFuncao(param)
+
+// expressão de função
+
+//const soma = function(num1, num2) {return num1 + num2}
+//console.log(soma(1, 1))
+
+// diferença principal: HOISTING
+// funções e var são "listadas" no topo do arquivo 
+
+console.log(apresentar())
+
+function apresentar() {
+    return "olá";
+}
+
+console.log(soma(1, 1))
+const soma = function(num1, num2) {
+        return num1 + num2}
