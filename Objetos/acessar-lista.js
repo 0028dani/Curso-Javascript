9730fcc3be0de3bbf958574646a552e52935747d
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-10-16 13:40:36 -0300

    Acessando chave dentro do objeto

---
+++ Objetos/acessar-lista.js
@@ -0,0 +1,12 @@
+const cliente = {
+    nome: "André",
+    idade: 36,
+    cpf: "123456789",
+    email: "andre@email.com"
+}
+
+//const chaves = ["nome", "idade", "cpf", "email"]
+
+console.log(cliente[chaves[0]])
+
+chaves.forEach(info => console.log(cliente[info]))
