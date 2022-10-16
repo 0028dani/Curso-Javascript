a969745c80803403723818b3518be6cb19b754e2
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-10-16 13:42:35 -0300

    Acrescentando chave no objeto

---
+++ Objetos/adicionando-campo.js
@@ -0,0 +1,16 @@
+const cliente = {
+    nome: "André",
+    idade: 36,
+    cpf: "123456789",
+    email: "andre@email.com"
+}
+
+console.log(cliente)
+
+cliente.fone = "92828282828"
+
+console.log(cliente)
+
+cliente.fone = "212121212121211"
+
+console.log(cliente)
