65ca7258b7a6291d1d7e8c038001601689d51035
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-10-16 12:41:38 -0300

    Utilização de template string

---
+++ template-strings.js
@@ -0,0 +1,11 @@
+const nome = "Ju";
+const idade = 2021-1981;
+const cidadeNascimento = "São Paulo";
+
+const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeNascimento;
+
+// template literal
+const apresentacao1 = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`
+
+console.log(apresentacao)
+console.log(apresentacao1)
