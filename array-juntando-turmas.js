6892003764c72d7cb3284eb5f302a153ac2085bd
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-10-16 12:46:56 -0300

    Array utilizando .concat

---
+++ array-juntando-turmas.js
@@ -0,0 +1,5 @@
+const salaDePhyton = [' Melissa', ' Helena', ' Rodrigo']
+const salaDeJavaScript = [' Ju', ' Leo', ' Raquel']
+
+const salaUnificadas = salaDePhyton.concat(salaDeJavaScript)
+console.log(salaUnificadas)
