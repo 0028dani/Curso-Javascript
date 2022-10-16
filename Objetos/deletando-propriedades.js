a29869c0388f741c81b2075bb9210138b4c9ef4c
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-10-16 13:43:19 -0300

    Deletando propriedades no objeto

---
+++ Objetos/deletando-propriedades.js
@@ -0,0 +1,25 @@
+const objPersonagem = {
+    nome: "Gandalf",
+    classe: "mago",
+    nivel: "20",
+    aliado: {
+      nome: "Saruman",
+      classe: "mago"
+    },
+    status: "desaparecido"
+   }
+
+   delete objPersonagem.aliado
+   delete objPersonagem["status"]//opção colchete
+
+   console.log(objPersonagem.aliado)
+   console.log(objPersonagem.status)
+   console.log(objPersonagem)
+
+
+
+const delProp = delete objPersonagem.aliado
+const delPropInexistente = delete objPersonagem["endereco"]
+
+console.log(delProp) //true
+console.log(delPropInexistente) //true
