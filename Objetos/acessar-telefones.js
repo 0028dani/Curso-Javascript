190ae6c188f4c454ef38b9403f983d719966722e
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-10-16 13:42:13 -0300

    Acessando lista de um objeto
    Acessando dois valores de uma chave pelo ForEach

---
+++ Objetos/acessar-telefones.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "André",
+    idade: 36,
+    cpf: "123456789",
+    email: "andre@email.com",
+    fones: ["129292992929", "1922928292929"]
+}
+
+cliente.fones.forEach(fone => console.log(fone))
