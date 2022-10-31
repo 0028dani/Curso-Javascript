1efc90ab5b65f3d50b19340d8de21df31848a36a
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-10-31 20:46:22 -0300

    Utilizando cadeia de prototipos

---
+++ Objetos/Cadeia-de-prototipos.js
@@ -0,0 +1,31 @@
+function Cliente (nome, cpf, email, saldo) {
+    this.nome = nome
+    this.cpf = cpf
+    this.email = email
+    this.saldo = saldo
+     this.depositar = function(valor){
+      this.saldo += valor
+    }
+   }
+   const andre = new Cliente("Andre", "12312312312", "andre@email.com", 100)
+
+
+   function ClientePoupanca(nome, cpf, email, saldo, saldoPoup){
+    Cliente.call(this, nome, cpf, email, saldo)
+    this.saldoPoup = saldoPoup
+   }
+   const ju = new ClientePoupanca("Ju", "12312312312", "ju@email.com", 100, 200)
+
+   ClientePoupanca.prototype.depositarPoup = function(valor){
+    this.saldoPoup += valor
+   }
+
+
+console.log(andre.hasOwnProperty("saldoPoup"))
+console.log(ju.hasOwnProperty("saldoPoup"))
+console.log(andre instanceof Cliente)
+console.log(typeof andre)
+console.log(typeof ju)
+console.log(ju instanceof ClientePoupanca)
+console.log(Function.prototype.isPrototypeOf(Cliente))
+console.log(Cliente.constructor === Function)
