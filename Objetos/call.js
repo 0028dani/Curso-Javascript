96a00e33e1330629337bea70809b54973bb590f9
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-11-03 16:34:44 -0300

    Usando call no object

---
+++ Objetos/call.js
@@ -0,0 +1,19 @@
+function imprimeNomeEmail(tipoCliente){
+    console.log(`${tipoCliente} - nome: ${this.nome}, email: ${this.email}`)
+   }
+   
+   const cliente1 = {
+    nome: "Carlos",
+    email: "c@email.com"
+   }
+   
+   const cliente2 = {
+    nome: "Fred",
+    email: "f@email.com"
+   }
+
+imprimeNomeEmail.call(cliente1, "cliente especial")
+// cliente especial - nome: Carlos, email: c@email.com
+
+imprimeNomeEmail.call(cliente2, "cliente estudante")
+// cliente estudante - nome: Fred, email: f@email.com
