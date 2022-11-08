1b7ae3c602dd8cbbb1aa9dd00e62f1b198c82900
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-11-07 22:29:44 -0300

    Utilização de Object.create

---
+++ Objetos/object-create.js
@@ -0,0 +1,11 @@
+const objPersonagem = {
+    nome: "Gandalf",
+    classe: "mago",
+    nivel: "20"
+   }
+   
+   const objPersonagem2 = Object.create(objPersonagem)
+   objPersonagem2.nome = "Gandalf, o Cinzento"
+   
+   console.log(objPersonagem.nome) //Gandalf
+   console.log(objPersonagem2.nome) //Gandalf, o Cinzento
