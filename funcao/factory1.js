e0b567dbe4c2309253de1ec480e96e063a9aee4a
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2023-01-13 13:23:20 -0300

    Utilização de factory

---
+++ funcao/factory1.js
@@ -0,0 +1,11 @@
+// Factory simples
+// Factory é uma função que retorna o objeto
+
+function criarPessoa(){
+    return {
+        nome: "Ana",
+        sobrenome: "Silva"
+    }
+}
+
+console.log(criarPessoa())
