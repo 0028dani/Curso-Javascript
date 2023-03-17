e7f6072fcafc8aea6384fe5132ca7c9ddf30a8da
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2023-03-17 14:39:17 -0300

    Utilização do closure

---
+++ funcao/closure.js
@@ -0,0 +1,17 @@
+// closure é o escopo criado quando uma função é declarada
+// Esse escopo permite a função acessar e manipular variáveis externas á função
+
+// Contexto léxico em ação
+
+const x = "Global";
+
+function fora(){
+    const x = "local";
+    function dentro(){
+        return x
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
