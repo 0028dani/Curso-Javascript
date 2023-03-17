638b8e27451ac02eb9fcd1b4ebb67b03dd314463
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2023-03-17 14:42:05 -0300

    Parametros

--- parametros.js
+++ parametros.js
@@ -2,7 +2,7 @@
 
                 //2    //2
 function soma(numero1, numero2){
-    return numero1 + numero2;
+    return "oi"// numero1 + numero2;
 }
 
 console.log(soma(2, 2))
@@ -17,7 +17,7 @@ function nomeIdade(nome, idade){
     return `meu nome é ${nome} e minha idade é ${idade}`
 }
 
-console.log(nomeIdade(40, "Juliana"))
+//console.log(nomeIdade(40, "Juliana"))
 
 // função dentro de uma função
 function soma1(numero1, numero2) {
@@ -28,7 +28,7 @@ function multiplica(numero1 =1, numero2=1) {
     return numero1 * numero2;
 }
 
-console.log(multiplica(soma(4, 5), soma(3,3)))
+//console.log(multiplica(soma(4, 5), soma(3,3)))
 
                             //9
-console.log(multiplica(soma1(4, 5)))
+//console.log(multiplica(soma1(4, 5)))
